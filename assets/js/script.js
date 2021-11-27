5f9b55799c0693787b51ac21304908c5ec4d0173
evelynfoy <evelyn.foy@hotmail.com>
2021-11-27 11:33:19 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function rumGame() {
+
+}
+
+function checkAnswer() {
+    
+}
+
+function calculateCorrectAnswer() {
+    
+}
+
+function incrementScore() {
+    
+}
+
+function icrementWrongAnswer() {
+    
+}
+
+function displayAdditionQuestion() {
+    
+}
+
+function displaySubtractQuestion() {
+    
+}
+
+function displayMultiplyQuestion() {
+    
+}
