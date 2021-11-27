50d235a27ecc2e41113f5680612c2088e0edc900
evelynfoy <evelyn.foy@hotmail.com>
2021-11-27 18:17:46 +0000

    Add code to check the answer and respond to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function(){
     for (let button of buttons){
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked submit")
+                checkAnswer();
             } else {
                 let gameType = this .getAttribute("data-type");
                 runGame(gameType);
@@ -31,6 +31,7 @@ function runGame(gameType) {
     if (gameType === "addition")
     {
         displayAdditionQuestion(num1, num2);
+        //let result = calculateCorrectAnswer(num1, num2, gameType);
     } else {
         // Unknown game type
         alert(`Unknown game type: ${gameType}`);
@@ -38,25 +39,50 @@ function runGame(gameType) {
     }
 
 }
-
+/**
+ * Checks the answer aginst the first element in 
+ * the returned calculatedCorrectAnswer array
+ */
 function checkAnswer() {
-    
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert("Hey! You got it right :D")
+    } else {
+        alert(`Awww....you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}`);
+    }
+
+    runGame(calculatedAnswer[1]);
 }
 
+/**
+ * Gets the operands (the numbers) and the operator (plus, minu etc) 
+ * directly from the dom, and returns the correct answer.
+ */
 function calculateCorrectAnswer() {
-    
+    let operand1 = parseInt(document.getElementById("operand1").innerText);
+    let operand2 = parseInt(document.getElementById("operand2").innerText);
+    let operator = document.getElementById("operator").innerText;
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
 }
 
 function incrementScore() {
     
 }
 
-function icrementWrongAnswer() {
+function incrementWrongAnswer() {
     
 }
 
 function displayAdditionQuestion(operand1, operand2) {
-    
+
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "+";
